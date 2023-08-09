860428360e4e23e26d8002129e394c8cdf1f295b
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2023-08-09 11:10:31 -0400

    Update MainSidebar.php

--- resources/views/layout/MainSidebar.php
+++ resources/views/layout/MainSidebar.php
@@ -75,15 +75,15 @@
                     </a>
                 </li>
 
-                <li class="nav-item">
-                    <a id="Dash" href="<?php echo $this->raiz; ?>/Tables/Boxes" class="nav-link">
+                <!-- <li class="nav-item">
+                    <a id="Dash" href="<?php //echo $this->raiz; ?>/Tables/Boxes" class="nav-link">
                         <i class="nav-icon fa fa-table"></i>
                         <p>
                             Caja
                             <span class="right badge badge-warning">Develop</span>
                         </p>
                     </a>
-                </li>
+                </li> -->
 
                 <!-- <li class="nav-item menu-closed">
                     <a href="#" class="nav-link">
