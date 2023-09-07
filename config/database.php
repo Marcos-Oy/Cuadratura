959e8067630972c74664e4fdb96b6ee640d5fdf6
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2023-09-07 10:40:59 -0300

    error503

--- config/database.php
+++ config/database.php
@@ -9,25 +9,39 @@ class Database {
     public $conn;
     
     public function getConnection() {
+
         $this->conn = null;
-        
+    
         try {
+    
             $tns = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = " . $this->host . ")(PORT = 1521)))(CONNECT_DATA=(SID=cuadratura)))";
+            
+            // Desactivar la configuración de errores antes de la conexión
+            ini_set('display_errors', 0);
+            error_reporting(0);
+            
             $this->conn = oci_connect($this->username, $this->password, $tns);
+            
+            // Restaurar la configuración de errores después de la conexión
+            ini_restore('display_errors');
+            error_reporting(E_ALL);
+    
             if (!$this->conn) {
                 $e = oci_error();
-                echo "Error de conexión: " . $e['message'];
-                phpinfo();
+                include __DIR__ . '/../resources/views/sessions/Error503.php';
                 exit;
+            } else {
+                return $this->conn;
             }
+    
         } catch(Exception $exception) {
-            echo "Error de conexión: " . $exception->getMessage();
-            phpinfo();
+            // Manejar cualquier otra excepción aquí si es necesario
+            $e = $exception->getMessage();
+            include __DIR__ . '/../resources/views/sessions/Error503.php';
             exit;
         }
-        
-        return $this->conn;
     }
+    
     
     public function closeConnection() {
         oci_close($this->conn);
