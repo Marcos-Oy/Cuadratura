6703bfd75726f0f77df8bcfbcab7eff152b84d21
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2023-09-13 09:32:15 -0300

    formateo main

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// En un archivo llamado "config.php"
-require_once "DatabaseConnection.php";
-
-$database = new Database();
-$conn = $database->getConnection();
-
-?>
--- resources/assets/js/functions.js
+++
@@ -1,1 +0,0 @@
-swal("Good job!", "You clicked the button!", "success");
